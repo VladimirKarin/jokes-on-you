89983fcb27f3178824e9d6a35aec1b8c54f49f2a
Vladimir Karin <Vladimir.Karin@gmail.com>
2023-03-03 14:20:32 +0200

    fetching

--- src/App.js
+++ src/App.js
@@ -1,14 +1,48 @@
-
+import JokeContent from './JokeComponent';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
+  const [items, setItems] = useState([]);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [error, setError] = useState(null);
 
-      </header>
-    </div>
-  );
+  useEffect(() => {
+    fetch('https://v2.jokeapi.dev/joke/Programming?amount=10')
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          setItems(result);
+        },
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      );
+  }, []);
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  } else if (!isLoaded) {
+    return <div>Loading...</div>;
+  } else {
+    return (
+      <ul>
+        {items.jokes.map((item) => {
+          if (item.type === 'single') {
+            return <li>{item.joke}</li>;
+          } else {
+            return (
+              <li>
+                {item.setup} {item.delivery}
+              </li>
+            );
+          }
+        })}
+      </ul>
+    );
+  }
 }
 
 export default App;
